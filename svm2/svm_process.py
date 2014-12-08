8d378e3d9cfc492f82b4377652851dff19774183
inachen <ina.w.chen@gmail.com>
2014-12-08 12:34:13 -0800

    sync

---
+++ svm2/svm_process.py
@@ -0,0 +1,21 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import collections
+from collections import namedtuple
+import csv
+import numpy as np
+import math
+import matplotlib.cm as cm
+from mpltools import style
+from mpltools import color
+import copy
+import os.path
+import scipy.stats as stats
+
+from glob import glob
+from Bio import SeqIO
+import cPickle as pickle
+
+# import data
